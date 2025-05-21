79c52f00873e6994fde7aeabf3d0570d12788f21
Shiva Kumar <your-github-shivakumar1b2d@gmail.com>
2025-05-21 05:34:43 +0530

    Fix: removed SRP submodule and re-added as a normal folder

---
+++ OCP/OCP/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+public abstract class PaymentMethod
+{
+    public abstract void ProcessPayment(double amount);
+}
+
+public class PayPal : PaymentMethod
+{
+    public override void ProcessPayment(double amount)
+    {
+        Console.WriteLine($"{amount} is processing in Paypal Mode");
+    }
+}
+
+public class GooglePay : PaymentMethod
+{
+    public override void ProcessPayment(double amount) =>  Console.WriteLine($"{amount} is proccessing in GooglePay Mode");
+}
+
+
+public class PaymentProessor
+{
+    public void MakePayment(PaymentMethod paymentmethod, double amount)
+    {
+        paymentmethod.ProcessPayment(amount);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        PaymentProessor pay = new PaymentProessor();
+        double amount = 500;
+        PaymentMethod ppay = new PayPal();
+        PaymentMethod gpay = new GooglePay();
+
+        pay.MakePayment(ppay, amount);
+        pay.MakePayment(gpay, amount);
+
+    }
+}
---
+++ SRP/SRP/Program.cs
@@ -0,0 +1,44 @@
+﻿//SOLID PRINCIPLE
+//Single Responsibility Principle
+
+using System;
+
+class UserManager
+{
+    public string ? UserName { get; set; }
+
+    public void CreateUser()
+    {
+        Console.WriteLine($"User {UserName} created");
+    }
+}
+class Login
+{
+    public void UserLogin(UserManager user)
+    {
+        Console.WriteLine($"User {user.UserName} loggedin @ {DateTime.Now}");
+    }
+}
+
+class Email
+{
+    public void EmailGen(UserManager user)
+    {
+        Console.WriteLine($"Welcome : {user.UserName}  ");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        UserManager name = new UserManager();
+        Login login = new Login();
+        Email email = new Email();
+
+        name.UserName = ("Shiva");
+        name.CreateUser();
+        login.UserLogin(name);
+        email.EmailGen(name);
+    }
+}
